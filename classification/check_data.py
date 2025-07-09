9e3a7a74b0860e3b2a98b03c96ca833b6a19d35f
bezalelshteren <a0556722346@gmail.com>
2025-07-09 16:15:05 +0300

    ghujcscsc c c c f

--- classification/check_data.py
+++ classification/check_data.py
@@ -2,13 +2,16 @@ from classification.train_a_model import train_a_model
 
 
 class check_data:
-    def __init__(self):
-        self.dict_result =
-    def p(dict_result: dict, to_check: dict):
+
+    def __init__(self,dict_result, to_check: dict):
+        self.dict_result = dict_result
+        self. to_check = to_check
+
+    def tests(self):
         temp = 1
         dict_probability = {}
-        for group, group_dict in dict_result.items():
-            for dic, parm in to_check.items():
+        for group, group_dict in self.dict_result.items():
+            for dic, parm in self.to_check.items():
                 if parm in group_dict.get(dic, {}):
                     temp *= group_dict[dic][parm]
                     dict_probability[group] = temp
@@ -16,4 +19,4 @@ class check_data:
                     raise "error"
         best_group = max(dict_probability, key=dict_probability.get)
         best_prob = dict_probability[best_group]
-        return dict_result, dict_probability, best_group, best_prob
+        return self.dict_result, dict_probability, best_group, best_prob
